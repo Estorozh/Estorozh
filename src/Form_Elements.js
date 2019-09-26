6f2748aeede0e0454af2a20ec626da8064ea8d2b
pazetic <mr.ahilis@gmail.com>
2019-09-26 15:08:40 +0300

    creting rate-button

--- src/Form_Elements.js
+++ src/Form_Elements.js
@@ -1,2 +1,3 @@
 import './pug/blocks/__elements/Form_Elements.scss'
+import './pug/blocks/__elements/rate-button/rate-button.js'
 
---
+++ src/pug/blocks/__elements/rate-button/rate-button.js
@@ -0,0 +1,4 @@
+let star = document.querySelectorAll('.rate-button__star');
+
+star[3].classList.add('rate-button__star--full');
+star[9].classList.add('rate-button__star--full');
