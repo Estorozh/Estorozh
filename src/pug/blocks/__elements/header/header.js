cce5d6f7d5e07ca8e2de7a8b852bef43753da022
pazetic <mr.ahilis@gmail.com>
2019-11-02 21:33:41 +0300

    change header and footer for small screen

--- src/pug/blocks/__elements/header/header.js
+++ src/pug/blocks/__elements/header/header.js
@@ -2,6 +2,6 @@ let burger = document.querySelectorAll('.burger');
 
 for(i=0; i<burger.length; i++) {
     burger[i].addEventListener('click', function(e) {
-        e.target.nextElementSibling.classList.toggle('show');
-    })
+        e.target.parentNode.classList.toggle('show');
+    });
 }
